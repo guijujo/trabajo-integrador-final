39c76322e3fb0771ef266f4d3fe29f1e79106398
Guillermo García <guijujo.25@gmail.com>
2023-10-10 08:26:43 -0300

    saludo sweetAlert2

--- js/master.js
+++ js/master.js
@@ -15,25 +15,22 @@ fetch("./datos/productos.json")
       }
       console.log(producto);
       productosIndicador.innerHTML += `
-        <button type="button" data-bs-target="#carruselCelulares" data-bs-slide-to="${(dataslide += 1)}" class="${activo}" aria-current="${
-        producto.ariaCurrent
-      }" aria-label="Slide ${(slide += 1)}"></button>
+        <button type="button" data-bs-target="#carruselCelulares" data-bs-slide-to="${(dataslide += 1)}" class="${activo}" aria-current="${producto.ariaCurrent
+        }" aria-label="Slide ${(slide += 1)}"></button>
         `;
       productosHTML.innerHTML += `
         
                         <div class="carousel-item ${activo}">
-                        <img src="${
-                          producto.imagen[0]
-                        }" class="d-block w-100 img-fluid" alt="${
-        producto.nombre
-      }">
+                        <img src="${producto.imagen[0]
+        }" class="d-block w-100 img-fluid" alt="${producto.nombre
+        }">
                         <div class="carousel-caption d-block d-md-block">
                         <div class="descripcion my-5 fs-6">
                         <h5>${producto.nombre}</h5>
                             </div>
                                 <a   id='${JSON.stringify(
-                                  producto
-                                )}' href= '#' class='btn btn-outline-primary d-block botonDetalle'>Ver detalle</a>
+          producto
+        )}' href= '#' class='btn btn-outline-primary d-block botonDetalle'>Ver detalle</a>
                             </div>
                         </div>
                         </div>
@@ -78,3 +75,15 @@ fetch("./datos/productos.json")
   .catch((error) => {
     console.log("Ha ocurrido en error " + error);
   });
+
+  let saludo = document.querySelector(".saludo");
+  saludo.addEventListener("click", function () {
+    Swal.fire({
+      title: '¡Saludo especial!',
+      text: 'Quiero dedicar unas lineas al profesor Ángel Daniel Fuentes por habernos enseñado un montón con mucha dedicación, le deseo una muy pronta recuperación y siga compartiendo todo ese gran conocimiento que tiene con sus alumnos y colegas. Que tenga buen código y siga fachero con ese buen master.css y tan funcional como el master.js',
+      imageUrl: 'https://www.helpargentina.org/images/default/ong/untref_logos_01_sinmarcas.jpg',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Custom image',
+    })
+  })
